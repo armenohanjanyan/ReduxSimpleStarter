a49722932775ca4d934c92b24c29d21ca1f59809
armen <armenohanjanyan@yahoo.com>
2018-09-11 10:42:31 -0700

    books reducer implemented

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import Booksreducer from './reducer_books'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: Booksreducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+   return [
+        {title: 'JS Good parts'},
+        {title: 'the dark tower'},
+        {title: 'harry Poter'},
+        {title: 'Eloq Ruby'}
+    ]
+}
