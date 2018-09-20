577b3858efde5a3c359dd98d605238dd8c6a0a5a
armen <armenohanjanyan@yahoo.com>
2018-09-20 10:18:30 -0700

    search-bar implemented

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import searchBar from '../containers/searchBar'
+import SearchBar from '../containers/searchBar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
